5da790032d84fb9c5f22338beb979a6dcdd4fda9
Arseniy <khudyakov.03@bk.ru>
2026-04-23 18:54:39 +0300

    BZR-93 implemented REST endpoints for authorization admin API

--- src/main/resources/db/changelog/v1.0.2/20260402-00-5-insert-mvp-actions.sql
+++ src/main/resources/db/changelog/v1.0.2/20260402-00-5-insert-mvp-actions.sql
@@ -7,8 +7,9 @@ values (1,'WRITE', 'Редактировать спейс', 'space', now(), now(
        (2, 'DELETE', 'Удалять спейс', 'space', now(), now()),
        (3, 'READ', 'Читать сообщения в чате', 'chat_messages', now(), now()),
        (4, 'WRITE', 'Писать в чат', 'chat_messages', now(), now()),
-       (5, 'ADD', 'Добавлять новых пользователей в спейс', 'space_user', now(), now()),
-       (6, 'DELETE', 'Кикать пользователей из спейса', 'space_user', now(), now()),
-       (7, 'READ', 'Смотреть роли и права всех пользователей', 'space_user_actions', now(), now()),
-       (8, 'WRITE', 'Изменять роли и права других пользоваталей', 'space_user_actions', now(), now());
+       (5, 'DELETE', 'Удалять любые сообщения', 'chat_messages', now(), now()),
+       (6, 'ADD', 'Добавлять новых пользователей в спейс', 'space_user', now(), now()),
+       (7, 'DELETE', 'Кикать пользователей из спейса', 'space_user', now(), now()),
+       (8, 'READ', 'Смотреть роли и права всех пользователей', 'space_user_actions', now(), now()),
+       (9, 'WRITE', 'Изменять роли и права других пользоваталей', 'space_user_actions', now(), now());
 
