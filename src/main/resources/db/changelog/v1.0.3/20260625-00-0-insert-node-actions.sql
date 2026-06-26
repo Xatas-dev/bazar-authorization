84b75e752c4b66c799322decef83c6c0be0c550f
paul <kulebyakinpaul@yandex.ru>
2026-06-26 22:19:32 +0300

    BZR-155 Добавить интеграцию с bazar-auth на bazar-storage

---
+++ src/main/resources/db/changelog/v1.0.3/20260625-00-0-insert-node-actions.sql
@@ -0,0 +1,4 @@
+insert into action(id, code, name, resource, resource_name, created_at, updated_at)
+values (12, 'UPLOAD', 'Загружать файлы', 'nodes', 'Файлы' ,now(), now()),
+       (13, 'DOWNLOAD', 'Скачивать файлы', 'nodes', 'Файлы' , now(), now()),
+       (14, 'DELETE', 'Удалять любые файлы', 'nodes', 'Файлы' , now(), now())
---
+++ src/main/resources/db/changelog/v1.0.3/20260625-00-1-insert-node-global-roles.sql
@@ -0,0 +1,2 @@
+insert into roles_actions (role_id, action_id, assigned_attribute, created_at, updated_at)
+values (1, 13, null, now(), now())
