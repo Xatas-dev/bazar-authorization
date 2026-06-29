6ac5d1a902129fc07566c0584ccc7ad0faee2d1d
paul <kulebyakinpaul@yandex.ru>
2026-06-30 00:04:35 +0300

    BZR-155 Добавил экшен READ и логику для владельца файла

--- src/main/resources/db/changelog/v1.0.3/20260625-00-0-insert-node-actions.sql
+++ src/main/resources/db/changelog/v1.0.3/20260625-00-0-insert-node-actions.sql
@@ -1,4 +1,8 @@
+--liquibase formatted sql
+--changeset PavelKulebyakin:1
+--description add storage node actions
 insert into action(id, code, name, resource, resource_name, created_at, updated_at)
-values (12, 'UPLOAD', 'Загружать файлы', 'nodes', 'Файлы' ,now(), now()),
-       (13, 'DOWNLOAD', 'Скачивать файлы', 'nodes', 'Файлы' , now(), now()),
-       (14, 'DELETE', 'Удалять любые файлы', 'nodes', 'Файлы' , now(), now())
+values (12, 'READ', 'Смотреть файлы в хранилище', 'storage_node', 'Хранилище' ,now(), now()),
+       (13, 'UPLOAD', 'Загружать файлы в хранилище', 'storage_node', 'Хранилище' ,now(), now()),
+       (14, 'DOWNLOAD', 'Скачивать файлы из хранилища', 'storage_node', 'Хранилище' , now(), now()),
+       (15, 'DELETE', 'Удалять любые файлы из хранилища', 'storage_node', 'Хранилище' , now(), now())
